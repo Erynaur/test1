b2822324d683b8b7234047f189e03f4eab903d58
Erynaur <68155949+Erynaur@users.noreply.github.com>
2024-11-26 14:39:50 +0100

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -6,7 +6,15 @@ if (!localStorage.getItem('cookies_accepted')) {
 document.getElementById('accept-cookies').addEventListener('click', function() {
     localStorage.setItem('cookies_accepted', 'true');
     document.getElementById('cookie-banner').style.display = 'none';  // Cache la bannière après acceptation
-    // Ajoute ici le code Google Analytics pour activer le suivi
+   <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QWJKS7VPFC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QWJKS7VPFC');
+</script>
     gtag('consent', 'update', { 'analytics_storage': 'granted' });
 });
 
