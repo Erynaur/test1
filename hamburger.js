24c01424c0767974ef0f6b041703b68a8146ec1f
Erynaur <68155949+Erynaur@users.noreply.github.com>
2024-11-26 17:28:39 +0100

    Create hamburger.js

---
+++ hamburger.js
@@ -0,0 +1,4 @@
+// Toggle menu when hamburger button is clicked
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    document.getElementById("navbar-list").classList.toggle("active");
+});
